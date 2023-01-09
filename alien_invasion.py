e03f2a4e15adaa6e80cc5956fcc51832adeadbb4
Michael Diekema <mdiekema@michaels-mini.lan>
2023-01-09 01:14:32 -0500

    Fixed main file merge

--- alien_invasion.py
+++ alien_invasion.py
@@ -2,7 +2,6 @@ import sys
 
 import pygame
 
-<<<<<<< HEAD
 from settings import Settings
 from ship import Ship
 import game_functions as gf
@@ -19,21 +18,5 @@ def run_game():
         gf.check_events(ship)
         ship.update()
         gf.update_screen(ai_settings, screen, ship)
-=======
-def run_game():
-    pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
-    pygame.display.set_caption("Alien Invasion")
-
-    bg_color = (230, 230, 230)
-
-    while True:
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                sys.exit()
-
-        screen.fill(bg_color)
-        pygame.display.flip()
->>>>>>> 72b3d1b6baa955205347a022994472b1d1693326
 
 run_game()
